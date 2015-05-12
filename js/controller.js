8d61011449eea327302b120ec5791f5c878f0f74
Jefferson Choi <jeffersonchoi@gmail.com>
2015-05-11 19:31:20 -0700

    working Xs and Os, cannot click twice on the same box

--- js/controller.js
+++ js/controller.js
@@ -11,6 +11,11 @@ angular
 		self.p2score=0;
 		self.startingName= true;
 		self.nameTyped = nameTyped;
+		self.p1Turn=p1Turn;
+		self.p2Turn=p2Turn;
+		self.whosTurn=whosTurn;
+		self.playercount=1;
+		self.boxes=[];
 
 
 		function nameTyped() {
@@ -18,10 +23,33 @@ angular
 		}
 
 
-		
+	function whosTurn(i) {
+		if (self.playercount %2 !== 0) {
+			self.playercount++;
+			self.p1Turn(i);
+		} else if (self.playercount %2 == 0) {
+			self.playercount++;
+			self.p2Turn(i);
+		}
+	}
 
+	function p1Turn(i) {
+		if (self.boxes[i] != "x" && self.boxes[i] != "o") {
+			self.boxes[i]="x";
+		} else {
+			alert("Please choose another box :)");
+			self.playercount--;
+		}
+	}
+	function p2Turn(i) {
+		if (self.boxes[i] != "x" && self.boxes[i] != "o") {
+			self.boxes[i]="o";
+		} else {
+			alert("Please choose another box :)");
+			self.playercount--;
+		}
 
-
+	}
 
 
 }
